ce26975d6c56d95b6d65c55aff16250937d68a5f
DHARAVATH USHARANI <ushadharavath786@gmail.com>
2023-03-15 19:47:06 +0530

    hi

---
+++ src/main/java/com/example/dependencyinjection/service/StudentServiceImpl.java
@@ -0,0 +1,42 @@
+package com.example.dependencyinjection.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.dependencyinjection.model.Student;
+
+@Service
+
+public class StudentServiceImpl implements StudentService {
+	private List<Student> studentList= new ArrayList<>();
+	@Override
+	public void add(Student student) {
+		// TODO Auto-generated method stub
+studentList.add(student) ;
+	}
+
+	@Override
+	public void delete(Student student) {
+		// TODO Auto-generated method stub
+		studentList.remove(student);
+	}
+
+	@Override
+	public List<Student> all() {
+		// TODO Auto-generated method stub
+		return studentList;
+	}
+
+	@Override
+	public Student findById(String id) {
+		for(Student student : studentList) {
+			if(student.getIdStudent().equals(id)) {
+				return student;
+			}
+		}
+		return null;
+	}
+
+}
